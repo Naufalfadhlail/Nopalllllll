aadbca26819eb2b3419408def47698ec258975a0
Naufalfadhlail <114450489+Naufalfadhlail@users.noreply.github.com>
2022-09-27 10:27:01 +0700

    tugas
    ini tugas scanner 5 september

---
+++ App.java
@@ -0,0 +1,35 @@
+package com.scanner;
+import java.util.Scanner;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        int value = scanner.nextInt();
+
+        //input 10 dan 5
+
+        Scanner scanner = new Scanner(System.in);
+
+        system.out.println("Program penjumlahan sangat"
+        sederhana");
+
+        System.out.print("Masukan Angka pertama : ");
+
+        int value = scanner.nextInt();
+
+        System.out.print("Masukan Angka kedua : ");
+
+        int anotherValue = scanner.nextInt();
+
+        int result = value + anotherValue;
+
+        System.out.println("Hasilnya adalah : " + result);
+        System.out.println( "Hello World!" );
+    }
+}
