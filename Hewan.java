b366291b5640ce23b4004cf8bb2a1a2e7dcb9dc8
Naufalfadhlail <114450489+Naufalfadhlail@users.noreply.github.com>
2022-10-04 09:11:51 +0700

    Add files via upload

---
+++ Hewan.java
@@ -0,0 +1,22 @@
+package com.hewan;
+
+public class Hewan {
+    String nama;
+    int berat;
+    int jumlahKaki;
+
+    public Hewan(String namaHewan){
+    nama = namaHewan;
+    }
+    public void beratHewan(int beratHewan){
+        berat = beratHewan;
+    }
+    public void jumlahKakiHewan(int jumlahKakiHewan){
+        jumlahKaki = jumlahKakiHewan;
+    }
+    public void cetakHewan(){
+        System.out.println("Nama hewan  : " +nama);
+        System.out.println("Berat hewan : " + berat + " kg");
+        System.out.println("Jumlah kaki : " +jumlahKaki );
+    }
+}
---
+++ Mainnnnnnnnn.java
@@ -0,0 +1,17 @@
+package com.hewan;
+
+
+public class Mainnnnnnnnn
+{
+    public static void main( String[] args )
+    {
+        Hewan Elang = new Hewan("Elang");
+        Hewan Kucing = new Hewan("Kucing");
+        Elang.beratHewan(5);
+        Elang.jumlahKakiHewan(2);
+        Elang.cetakHewan();
+        Kucing.beratHewan(2);
+        Kucing.jumlahKakiHewan(4);
+        Kucing.cetakHewan();
+    }
+}
